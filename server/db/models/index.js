5f9ef044e3590469f6559338ebe367e8402d91c4
alvinjtang <alvintangdesigns@gmail.com>
2019-02-27 15:34:09 -0500

    establishes associations

---
+++ server/db/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./user')
+const Pizza = require('/pizza')
+
+Pizza.belongsToMany(User, {through: 'order'})
+User.belongsToMany(Pizza, {through: 'order'})
+
+module.exports = {
+  User
+}
