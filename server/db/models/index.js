68094a84407244bf1e92d93238799970a608fce3
alvinjtang <alvintangdesigns@gmail.com>
2019-02-27 15:03:33 -0500

    BACKEND: defines User model; defines Pizza model; creates many-to-many association between User and Pizza

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,8 @@
 const User = require('./user')
+const Pizza = require('./pizza')
+
+User.belongsToMany(Pizza, {through: 'order'})
+Pizza.belongsToMany(User, {through: 'order'})
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +18,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Pizza
 }
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,26 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
+    allowNull: false,
+    isEmail: true
+  },
+  address: {
+    type: Sequelize.STRING,
     allowNull: false
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  favorite: {
+    type: Sequelize.STRING
   },
   password: {
     type: Sequelize.STRING,
