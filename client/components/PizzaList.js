b33757dea9d5930c155048aeb90103b23bba24f0
alvinjtang <alvintangdesigns@gmail.com>
2019-02-27 17:27:20 -0500

    debug: allPizza component

--- client/components/PizzaList.js
+++ client/components/PizzaList.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getPizzas} from '../store/pizzas'
+import {SinglePizza} from './index'
+import {withRouter} from 'react-router-dom'
 
 class PizzaList extends Component {
   componentDidMount() {
+    console.log('INSIDE PIZZA LIST')
     this.props.getPizzas()
   }
   render() {
     return (
       <div>
+        TEST
         {this.props.pizzas.map(pizza => {
-          return <div>PIZZA HERE</div>
+          return <SinglePizza key={pizza.id} pizza={pizza} />
         })}
       </div>
     )
@@ -23,4 +27,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {getPizzas})(PizzaList)
+export default withRouter(connect(mapStateToProps, {getPizzas})(PizzaList))
---
+++ client/components/SinglePizza.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SinglePizza = props => {
+  console.log(props.pizza)
+  return (
+    <div>
+      <h1>TEST</h1>
+    </div>
+  )
+}
+
+export default SinglePizza
