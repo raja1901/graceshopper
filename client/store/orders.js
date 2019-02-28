94f879cae1dd493b956df788689dc4e8b1605c91
alvinjtang <alvintangdesigns@gmail.com>
2019-02-28 15:54:57 -0500

    added post thunk, updated PizzaList, Add to Cart button functionality

--- client/store/orders.js
+++ client/store/orders.js
@@ -2,10 +2,16 @@ import axios from 'axios'
 
 // ACTION TYPES
 const GET_ORDERS = 'GET_ORDERS'
+const ADD_ORDER = 'ADD_ORDER'
 
 // ACTION CREATORS
 const gotOrders = payload => ({
   type: GET_ORDERS,
+  payload
+})
+
+const addOrder = payload => ({
+  type: ADD_ORDER,
   payload
 })
 
@@ -19,6 +25,15 @@ export const getOrders = cartId => async dispatch => {
   }
 }
 
+export const createOrder = (cartId, pizza) => async dispatch => {
+  try {
+    const {data} = await axios.post(`/api/orders/${cartId}`, pizza)
+    dispatch(addOrder(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 // INITIAL STATE
 const initialState = []
 
@@ -27,6 +42,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_ORDERS:
       return action.payload
+    case ADD_ORDER:
+      return state.push(action.payload)
     default:
       return state
   }
