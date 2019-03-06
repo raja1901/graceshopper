9629a0d97a71411981a7b04a7063b8e40bdcdcb9
alvinjtang <alvintangdesigns@gmail.com>
2019-03-06 02:45:53 -0500

    default route homepage

--- client/routes.js
+++ client/routes.js
@@ -32,7 +32,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route exact path="/" component={PizzaList} />
+        <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/allpizzas" component={PizzaList} />
